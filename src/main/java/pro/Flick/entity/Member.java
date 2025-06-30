a41e076aa1ab0c057fc1680e1b8a65456506b2f5
MKSonny <minson9@naver.com>
2025-06-30 14:02:59 +0900

    주석 추가

--- src/main/java/pro/Flick/entity/Member.java
+++ src/main/java/pro/Flick/entity/Member.java
@@ -15,7 +15,7 @@ public class Member {
     @Id @GeneratedValue
     private Long id;
 
-    private String username;
-    private String email;
+    private String username; // 유니크 제약 조건 추가 필요
+    private String email; // 유니크 제약 조건 추가 필요
     private LocalDateTime createTime;
 }
