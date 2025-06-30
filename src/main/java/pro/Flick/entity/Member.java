b204449da74925688890d81539a9a9d346753529
MKSonny <minson9@naver.com>
2025-06-30 14:15:50 +0900

    멤버 데이터 추가 테스트

--- src/main/java/pro/Flick/entity/Member.java
+++ src/main/java/pro/Flick/entity/Member.java
@@ -5,11 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Member {
 
     @Id @GeneratedValue
@@ -17,5 +20,13 @@ public class Member {
 
     private String username; // 유니크 제약 조건 추가 필요
     private String email; // 유니크 제약 조건 추가 필요
+
+    @CreatedDate
     private LocalDateTime createTime;
+
+
+    public Member(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
 }
--- src/test/java/pro/Flick/FlickApplicationTests.java
+++ src/test/java/pro/Flick/FlickApplicationTests.java
@@ -1,13 +1,30 @@
 package pro.Flick;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
+import pro.Flick.entity.Member;
 
 @SpringBootTest
 class FlickApplicationTests {
 
+	@Autowired
+	EntityManager em;
+
 	@Test
 	void contextLoads() {
+
 	}
 
+	@Test
+	@Transactional
+	@Commit
+	void memberEntityTest() {
+		Member member = new Member("hello", "hello@email.com");
+		em.persist(member);
+	}
 }
