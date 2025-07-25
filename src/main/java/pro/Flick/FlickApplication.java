d71596ca80f0643d375524ee9876204594bdde38
MKSonny <minson9@naver.com>
2025-07-25 10:23:09 +0900

    동영상) 오류> Video 엔티티에 member_id가 저장되지 않음 -> PostConstruct가 FileController, MemberController에 각각 있었는데 FileController의 PostConstruct가 먼저 실행되면서 memberRepository.findMember가 null을 반환함 -> DbInit 내부에 saveMemberAndVideo를 Transactional로 하여 저장 순서 보장되도록 함

--- src/main/java/pro/Flick/FlickApplication.java
+++ src/main/java/pro/Flick/FlickApplication.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import pro.Flick.controller.DbInit;
 import pro.Flick.entity.Member;
 import pro.Flick.repsository.FileRepository;
 import pro.Flick.repsository.MemberRepository;
@@ -12,8 +13,7 @@ import pro.Flick.repsository.MemberRepository;
 @SpringBootApplication
 @RequiredArgsConstructor
 public class FlickApplication {
-	private final MemberRepository memberRepository;
-	private final FileRepository fileRepository;
+	private final DbInit dbInit;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlickApplication.class, args);
@@ -21,14 +21,6 @@ public class FlickApplication {
 
 	@PostConstruct
 	public void init() {
-		memberRepository.save("HelloWorld", "Email", "123");
-		memberRepository.save("test", "Email2", "123");
-
-
-		Member member = memberRepository.findMember("Email", "123");
-		Member member2 = memberRepository.findMember("Email2", "123");
-		fileRepository.saveVideo("myVideo", "http://127.0.0.1:8080/video/test.mov", member);
-		fileRepository.saveVideo("myVideo2", "http://127.0.0.1:8080/video/test2.mov", member);
-		fileRepository.saveVideo("myVideo3", "http://127.0.0.1:8080/video/test3.mov", member2);
+		dbInit.saveMemberAndVideo();
 	}
 }
---
+++ src/main/java/pro/Flick/controller/DbInit.java
@@ -0,0 +1,30 @@
+package pro.Flick.controller;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pro.Flick.entity.Member;
+import pro.Flick.repsository.FileRepository;
+import pro.Flick.repsository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+public class DbInit {
+    private final MemberRepository memberRepository;
+    private final FileRepository fileRepository;
+
+    @Transactional
+    public void saveMemberAndVideo() {
+        memberRepository.save("HelloWorld", "Email", "123");
+        memberRepository.save("test", "Email2", "123");
+
+
+        Member member = memberRepository.findMember("Email", "123");
+        Member member2 = memberRepository.findMember("Email2", "123");
+        fileRepository.saveVideo("myVideo", "http://127.0.0.1:8080/video/test.mov", member);
+        fileRepository.saveVideo("myVideo2", "http://127.0.0.1:8080/video/test2.mov", member);
+        fileRepository.saveVideo("myVideo3", "http://127.0.0.1:8080/video/test3.mov", member2);
+    }
+}
