a9b6af17c7a22a1e199e533237a4fb4c8fef063a
MKSonny <minson9@naver.com>
2025-08-21 13:16:34 +0900

    test(Follow)
    새로 추가된 FollowService의 안정성을 보장하기 위해 통합 테스트 코드를 작성했습니다. 이 테스트는 기능의 정확성뿐만 아니라, Hibernate Statistics API를 활용하여 각 로직이 의도한 대로 단 1개의 SQL 쿼리만으로 실행되는지를 검증합니다.

---
+++ src/test/java/pro/Flick/follow/FollowServiceTest.java
@@ -0,0 +1,55 @@
+package pro.Flick.follow;
+
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
+import org.hibernate.SessionFactory;
+import org.hibernate.stat.Statistics;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import pro.Flick.entity.Member;
+import pro.Flick.member.MemberService;
+import pro.Flick.repsository.MemberRepository;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class FollowServiceTest {
+
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    FollowService followService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    private Statistics statistics;
+
+    @BeforeEach
+    void setUp() {
+        // 각 테스트가 시작되기 전에 SessionFactory에서 Statistics 객체를 가져오고 초기화합니다.
+        statistics = em.getEntityManagerFactory().unwrap(SessionFactory.class).getStatistics();
+        statistics.clear();
+    }
+
+
+    @Test
+    void addFollowAndDeleteTest() {
+        Member memberA = memberRepository.save(new Member("memberA", "emailA", "123"));
+        Member memberB = memberRepository.save(new Member("memberB", "emailB", "123"));
+
+        statistics.clear();
+
+        followService.memberAFollowsMemberB(memberA, memberB);
+        assertThat(statistics.getPrepareStatementCount()).isEqualTo(1);
+
+        statistics.clear();
+
+        followService.deleteFollower(memberA.getId(), memberB.getId());
+        assertThat(statistics.getPrepareStatementCount()).isEqualTo(1);
+    }
+}
